effb9e57e1b75c543fbde0fb4e074624538bae9c
ralvescosta <rafael.rac.mg@gmail.com>
2025-06-18 05:37:51 -0300

    Refactor backend configurations to consolidate S3 settings and remove redundant backend.tfvars files; add new database subnet module files.

--- day_01/terraform/environment/prod/backend.tf
+++ day_01/terraform/environment/prod/backend.tf
@@ -1,5 +1,7 @@
 terraform {
   backend "s3" {
-    
+    bucket = "linuxtips-uncomplicating-eks-tf-state"
+    key    = "eks/day-01/prod/state"
+    region = "us-east-1"
   }
 }
--- day_01/terraform/environment/prod/backend.tfvars
+++
@@ -1,3 +0,0 @@
-bucket = "linuxtips-uncomplicating-eks-tf-state"
-key = "eks/day-01/prod/state"
-region = "us-east-1"
