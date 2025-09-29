2601ab1e1192a790797d8382883ba18e47ce48bf
ralvescosta <rafael.rac.mg@gmail.com>
2025-09-29 09:44:49 -0300

    Refactor EKS configuration by updating module references and adding output variables

--- eks_vanila/data.tf
+++ eks_vanila/data.tf
@@ -3,11 +3,3 @@ data "aws_eks_cluster_auth" "default" {
 }
 
 data "aws_caller_identity" "current" {}
-
-output "account_id" {
-  value = data.aws_caller_identity.current.account_id
-}
-
-output "caller_arn" {
-  value = data.aws_caller_identity.current.arn
-}
