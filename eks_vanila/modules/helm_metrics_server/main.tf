4f429b9d8e4d3f1183836cf18d6025d7bcb4a69b
ralvescosta <rafael.rac.mg@gmail.com>
2025-10-18 10:07:50 -0300

    feat: Refactor metrics server Helm release configuration and remove deprecated security context

--- eks_vanila/modules/helm_metrics_server/main.tf
+++ eks_vanila/modules/helm_metrics_server/main.tf
@@ -1,28 +1,27 @@
 resource "helm_release" "metrics_server" {
   name       = "metrics-server"
-  repository = "oci://registry-1.docker.io/bitnamicharts"
-  chart      = "metrics-server"
   namespace  = "kube-system"
+  repository = "https://kubernetes-sigs.github.io/metrics-server/"
+  chart      = "metrics-server"
+  version    = "3.13.0"
 
   wait    = true
   replace = true
-  version = "7.4.12"
 
   values = [<<-YAML
     apiService:
       create: true
-    metricLabelsAllowlist:
-      - nodes=[*]
-    metricAnnotationsAllowList:
-      - nodes=[*]
-    commonLabels:
-      app.kubernetes.io/name: metrics-server
-      app.kubernetes.io/instance: metrics-server
-      app.kubernetes.io/version: "5.10.2"
-      app.kubernetes.io/managed-by: Helm
-      helm.sh/chart: metrics-server-7.4.12
-      meta.helm.sh/release-name: metrics-server
-      meta.helm.sh/release-namespace: kube-system
+    containerPort: 10251
+    hostNetwork:
+      enabled: true
+    defaultArgs:
+      - --cert-dir=/tmp
+      - --kubelet-preferred-address-types=InternalIP,ExternalIP,Hostname
+      - --kubelet-use-node-status-port
+      - --secure-port=10251
+      - --metric-resolution=15s
+      - --kubelet-insecure-tls
+    replicas: 2
   YAML
   ]
 }
