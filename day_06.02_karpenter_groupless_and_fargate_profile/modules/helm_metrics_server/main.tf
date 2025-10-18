7bf74a428907208ee571666311479d959ac1f879
ralvescosta <rafael.rac.mg@gmail.com>
2025-10-18 09:02:59 -0300

    feat: Update metrics server Helm release configuration for improved deployment

--- day_06.02_karpenter_groupless_and_fargate_profile/modules/helm_metrics_server/main.tf
+++ day_06.02_karpenter_groupless_and_fargate_profile/modules/helm_metrics_server/main.tf
@@ -1,28 +1,19 @@
 resource "helm_release" "metrics_server" {
   name       = "metrics-server"
-  repository = "oci://registry-1.docker.io/bitnamicharts"
-  chart      = "metrics-server"
   namespace  = "kube-system"
+  repository = "https://kubernetes-sigs.github.io/metrics-server/"
+  chart      = "metrics-server"
+  version    = "3.13.0"
 
   wait    = true
   replace = true
-  version = "7.4.12"
 
   values = [<<-YAML
     apiService:
       create: true
-    metricLabelsAllowlist:
-      - nodes=[*]
-    metricAnnotationsAllowList:
-      - nodes=[*]
-    commonLabels:
-      app.kubernetes.io/name: metrics-server
-      app.kubernetes.io/instance: metrics-server
-      app.kubernetes.io/version: "5.10.2"
-      app.kubernetes.io/managed-by: Helm
-      helm.sh/chart: metrics-server-7.4.12
-      meta.helm.sh/release-name: metrics-server
-      meta.helm.sh/release-namespace: kube-system
+    hostNetwork:
+      enabled: true
+    replicas: 2
   YAML
   ]
 }
