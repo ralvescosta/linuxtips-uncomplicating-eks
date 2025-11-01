eea76edd09b0b9b840022a9b35b474cee174cb8a
ralvescosta <rafael.rac.mg@gmail.com>
2025-11-01 08:50:22 -0300

    fix: Remove eks_certificate_authority output from EKS configuration

--- day_05.02_karpenter_and_advanced_autoscaling/output.tf
+++ day_05.02_karpenter_and_advanced_autoscaling/output.tf
@@ -6,10 +6,6 @@ output "caller_arn" {
   value = data.aws_caller_identity.current.arn
 }
 
-output "eks_certificate_authority" {
-  value = module.eks.cluster_certificate_authority_data
-}
-
 output "eks_endpoint" {
   value = module.eks.cluster_endpoint
 }
